4d931a4d20cf7bf4093385c721fcdd0d24796e3c
dmytro.prushchak <dmytro.prushchak@oracle.com>
2022-06-22 17:09:51 +0300

    feat: init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { Routes, Route} from "react-router-dom";
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import { getPosts } from './actions/posts';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import useStyles from './styles'
 
-function App() {
+import logo from './images/logo.png'
+
+const App = () => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  const [currentId, setCurrentId] = useState(null);
+
+  useEffect(() => {
+    dispatch(getPosts());
+
+  }, [currentId, dispatch])
+
+
+  const Home = () => {
+    return (
+      <Container maxidth="lg">
+        <AppBar className={classes.appBar} position="static" color="inherit">
+          <Typography className={classes.heading} variant="h2" align='center'>
+            Memories
+          </Typography>
+          <img className={classes.image} src={logo} alt='memories' height="60"></img>
+        </AppBar>
+        <Grow in>
+          <Container>
+              <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems='stretch' spacing={3}>
+                <Grid item xs={12} sm={7}>
+                  <Posts setCurrentId={setCurrentId}/>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Form currentId={currentId} setCurrentId={setCurrentId}/>
+                </Grid>
+              </Grid>
+          </Container>
+        </Grow>
+      </Container>
+    )
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
-  );
-}
+  )
+};
 
 export default App;
